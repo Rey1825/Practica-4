74878c88c9fc782e0a8b6485d855ba6c06abc1be
reyda <reyda@LAPTOP-ALH488S6>
2023-11-03 10:36:38 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Practica_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            label4 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Font = new Font("Stencil", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(39, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(772, 49);
+            label1.TabIndex = 0;
+            label1.Text = "Escribir numeros en orden inverso";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(56, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(165, 35);
+            label2.TabIndex = 1;
+            label2.Text = "Número A:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(60, 202);
+            label3.Name = "label3";
+            label3.Size = new Size(161, 35);
+            label3.TabIndex = 2;
+            label3.Text = "Número B:";
+            label3.Click += label3_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(55, 349);
+            label5.Name = "label5";
+            label5.Size = new Size(166, 35);
+            label5.TabIndex = 4;
+            label5.Text = "Número D:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(228, 128);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(187, 43);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(228, 199);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(187, 43);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox3.Location = new Point(228, 276);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(187, 43);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(228, 346);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(187, 43);
+            textBox4.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(436, 131);
+            label6.Name = "label6";
+            label6.Size = new Size(166, 35);
+            label6.TabIndex = 9;
+            label6.Text = "Número D:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(54, 279);
+            label4.Name = "label4";
+            label4.Size = new Size(167, 35);
+            label4.TabIndex = 3;
+            label4.Text = "Número C:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(435, 202);
+            label7.Name = "label7";
+            label7.Size = new Size(167, 35);
+            label7.TabIndex = 10;
+            label7.Text = "Número C:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(441, 279);
+            label8.Name = "label8";
+            label8.Size = new Size(161, 35);
+            label8.TabIndex = 11;
+            label8.Text = "Número B:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(437, 349);
+            label9.Name = "label9";
+            label9.Size = new Size(165, 35);
+            label9.TabIndex = 12;
+            label9.Text = "Número A:";
+            // 
+            // textBox5
+            // 
+            textBox5.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox5.Location = new Point(601, 128);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(187, 43);
+            textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            textBox6.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox6.Location = new Point(601, 199);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(187, 43);
+            textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            textBox7.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox7.Location = new Point(601, 276);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(187, 43);
+            textBox7.TabIndex = 15;
+            // 
+            // textBox8
+            // 
+            textBox8.Font = new Font("Rockwell", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox8.Location = new Point(601, 346);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(187, 43);
+            textBox8.TabIndex = 16;
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.BackColor = SystemColors.InactiveCaption;
+            button1.Font = new Font("Rockwell", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(322, 405);
+            button1.Name = "button1";
+            button1.Size = new Size(242, 51);
+            button1.TabIndex = 17;
+            button1.Text = "Invertir Orden";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(860, 468);
+            Controls.Add(button1);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label6;
+        private Label label4;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Button button1;
+    }
+}
